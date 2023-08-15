46f15cd20c5dfdfa67c40276edbeb0d1f2aa84c2
Severiano Sisneros <seve.sisneros@protonmail.com>
2023-08-14 22:27:20 -0700

    Tests passing.
    Signed-off-by: Severiano Sisneros <seve.sisneros@protonmail.com>

--- src/puzzles_contract.sol
+++ src/puzzles_contract.sol
@@ -2,24 +2,17 @@
 
 pragma solidity ^0.8.19;
 
+import "./puzzles_lib.sol";
+
 contract PuzzleGame {
-    struct Puzzle {
-        string puzzleType;
-        string data;
-        address author;
-        uint[2] solutionCommitment;
-        uint[2] verificationKey;
-        uint maxSolvers;
-        address [] solvers;
-        mapping(address => bool) solverMap;
-    }
 
     uint public puzzleCount = 0;
-    mapping(uint => Puzzle) public puzzles;
+    mapping(uint => PuzzleLib.Puzzle) public puzzles;
+    mapping(uint => bool) public puzzleSolved;
 
     function createPuzzle(string calldata puzzleType, string calldata data, uint[2] calldata solutionCommitment, uint256 maxSolvers) public {
         // Initialize a new puzzle
-        Puzzle storage newPuzzle = puzzles[puzzleCount];
+        PuzzleLib.Puzzle storage newPuzzle = puzzles[puzzleCount];
 
         // Set puzzle data
         newPuzzle.puzzleType = puzzleType;
@@ -29,6 +22,9 @@ contract PuzzleGame {
         newPuzzle.verificationKey = [uint(0x0000000000000000000000000000000000000000000000000000000000000001), uint(0x0000000000000000000000000000000000000000000000000000000000000002)];
         newPuzzle.maxSolvers = maxSolvers;
 
+        // Emit an event indicating that puzzle has been created
+        emit PuzzleCreated(puzzleCount);
+
         // Increment contract puzzle count
         puzzleCount++;
     }
@@ -36,75 +32,36 @@ contract PuzzleGame {
 
     function submitProof(uint id, uint[3] calldata proof) public {
         // Get puzzle data from storage
-        Puzzle storage puzzle = puzzles[id];
+        PuzzleLib.Puzzle storage puzzle = puzzles[id];
 
         // Check if puzzle has been solved by the caller
-        require(puzzle.solverMap[msg.sender] == false, "Solver has already solved this puzzle");
+        uint digest = uint(keccak256(abi.encodePacked(msg.sender, id)));
+        require(puzzleSolved[digest] == false, "Solver has already solved this puzzle");
 
         // Check that the puzzle is still available to solved
-        require(puzzle.solvers.length < puzzle.maxSolvers, "Maximum number of solvers reached");
+        require(puzzle.numSolvers < puzzle.maxSolvers, "Maximum number of solvers reached");
 
         // Verify the proof
-        require(verifyProof(proof, id), "Proof verification failed"); 
+        require(PuzzleLib.verifyProof( puzzle.solutionCommitment, puzzle.verificationKey, proof ), "Proof verification failed"); 
 
-        // Add the solver to the puzzle solvers list
-        puzzle.solvers.push(msg.sender);
+        // Emit an event to notify the puzzle has been solved by the caller
+        emit PuzzleSolved(id, msg.sender);
+
+        // Increment the number of solvers
+        puzzle.numSolvers++;
 
         // Set the solver as having solved the puzzle
-        puzzle.solverMap[msg.sender] = true;
+        puzzleSolved[digest] = true;
     }
 
-    function verifyProof( uint[3] memory proof, uint puzzleId) internal view returns (bool) {
-        // Get puzzle commitment and key from storage
-        uint[2] memory solutionCommitment = puzzles[puzzleId].solutionCommitment;
-        uint[2] memory g = puzzles[puzzleId].verificationKey;
-
-        // Initialize variables
-        uint[2] memory a;
-        uint z;
-
-        // Extract A and z from proof
-        a[0] = proof[0];
-        a[1] = proof[1];
-        z = proof[2];
-
-        // Compute Fiat-Shamir challenge, e
-        uint e = uint(sha256(abi.encode(g, solutionCommitment, a)));
-        
-         // lhs = a + [e]solutionCommitment
-        solutionCommitment = callBn256ScalarMul(solutionCommitment[0], solutionCommitment[1], e);
-        uint[2] memory lhs = callBn256Add(solutionCommitment[0], solutionCommitment[1], a[0], a[1]);
-
-        //rhs = [z]g
-        uint[2] memory rhs = callBn256ScalarMul(g[0], g[1], z);
-        return (lhs[0] == rhs[0] && lhs[1] == rhs[1]);
+    function getPuzzle(uint id) public view returns (PuzzleLib.Puzzle memory) {
+        return puzzles[id];
     }
 
-    // Bn256Add precompile
-    function callBn256Add(uint ax, uint ay, uint bx, uint by) public view returns (uint[2] memory result) {
-        uint[4] memory input;
-        input[0] = ax;
-        input[1] = ay;
-        input[2] = bx;
-        input[3] = by;
-        assembly {
-            if iszero(staticcall(gas(), 0x06, input, 0x80, result, 0x40)){
-                revert(0,0)
-            }
-        }
-    }
+    // Event to notify when a puzzle with id is created
+    event PuzzleCreated(uint id);
 
-    // Bn256ScalarMul precompile
-    function callBn256ScalarMul(uint x, uint y, uint scalar) public view returns (uint[2] memory result) {
-        uint[3] memory input;
-        input[0] = x;
-        input[1] = y;
-        input[2] = scalar;
-        assembly {
-            if iszero(staticcall(gas(), 0x07, input, 0x60, result, 0x40)) {
-                revert(0,0)
-            }
-        }
-    }
+    // Event to notify the puzzle with id has been solved by the caller (solver)
+    event PuzzleSolved(uint id, address solver);
 }
 
--- test/puzzles_contract.t.sol
+++ test/puzzles_contract.t.sol
@@ -1,25 +1,85 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.10;
 
-import {Test, console2} from "forge-std/Test.sol";
+import "forge-std/Test.sol";
 import {PuzzleGame} from "../src/puzzles_contract.sol";
+import {PuzzleLib} from "../src/puzzles_lib.sol";
 
 contract PuzzleGameTest is Test {
     PuzzleGame public puzzleGame;
 
     function setUp() public {
         puzzleGame = new PuzzleGame();
+        // struct Puzzle {
+        //     string puzzleType;
+        //     string data;
+        //     address author;
+        //     uint[2] solutionCommitment;
+        //     uint[2] verificationKey;
+        //     uint maxSolvers;
+        //     address [] solvers;
+        //     mapping(address => bool) solverMap;
+        // }
     }
 
     function test_createPuzzle() public {
+        string memory puzzleType = "crossword_type";
+        string memory puzzleData = "hints";
+        uint[2] memory verificationKey = [uint(0x0000000000000000000000000000000000000000000000000000000000000001), 
+                                          uint(0x0000000000000000000000000000000000000000000000000000000000000002)];
+        uint[2] memory solutionCommitment = [uint(0x0e074fdf0466838ef0a305e5718b96bd7481e02a5e1106ae7a80236f05db8fb7), 
+                                             uint(0x192f7ca3094b5962f681bb17e18d9bfad6235aa57f3ea2cc4dcdd6c7335f897c)];
+        uint maxSolvers = 1;
+
+        // vm.expectEmit(true, false, false, true);
+        puzzleGame.createPuzzle(puzzleType, puzzleData, solutionCommitment, maxSolvers);
+        
+        PuzzleLib.Puzzle memory puzzle = puzzleGame.getPuzzle(0);
+
+        assertEq(puzzleGame.puzzleCount(), 1);
+        assertEq(puzzle.puzzleType, puzzleType);
+        assertEq(puzzle.data, puzzleData);
+        assertEq(puzzle.author, address(this));
+        assertEq(puzzle.solutionCommitment[0], solutionCommitment[0]);
+        assertEq(puzzle.solutionCommitment[1], solutionCommitment[1]);
+        assertEq(puzzle.verificationKey[0], verificationKey[0]);
+        assertEq(puzzle.verificationKey[1], verificationKey[1]);
+        assertEq(puzzle.maxSolvers, maxSolvers);
+        assertEq(puzzle.numSolvers, 0);
+    }
+    // test fiat shamir challenge
+    function test_fiatShamirChallenge() public {
+        uint[2] memory verificationKey = [uint(0x0000000000000000000000000000000000000000000000000000000000000001), 
+                                          uint(0x0000000000000000000000000000000000000000000000000000000000000002)];
+        uint[2] memory solutionCommitment = [uint(0x0e074fdf0466838ef0a305e5718b96bd7481e02a5e1106ae7a80236f05db8fb7), 
+                                             uint(0x192f7ca3094b5962f681bb17e18d9bfad6235aa57f3ea2cc4dcdd6c7335f897c)];
+        uint[2] memory a = [uint(0x0000000000000000000000000000000000000000000000000000000000000001),
+                            uint(0x0000000000000000000000000000000000000000000000000000000000000002)];
+        uint e = PuzzleLib.fiatShamirChallenge(verificationKey, solutionCommitment, a);
+
+        assertEq(e, 0x09d37f4c7d21480eaa8fe6a4a6d4125ccc8945be34619d9ae3524932a4ef2947);
+    }
+                      
+    // test submitting a "trivial" proof, i.e. no blinding from the solver
+    function test_submitTrivialProof() public {
         string memory puzzleType = "crossword_type";
         string memory puzzleData = "hints";
         uint[2] memory solutionCommitment = [uint(0x0e074fdf0466838ef0a305e5718b96bd7481e02a5e1106ae7a80236f05db8fb7), 
                                              uint(0x192f7ca3094b5962f681bb17e18d9bfad6235aa57f3ea2cc4dcdd6c7335f897c)];
         uint maxSolvers = 1;
         puzzleGame.createPuzzle(puzzleType, puzzleData, solutionCommitment, maxSolvers);
-        assertEq(puzzleGame.puzzleCount(), 1);
-        // assertEq(puzzleGame.puzzles(1).puzzleType(), "test");
+
+        uint puzzleId = 0;
+        uint[3] memory puzzleProof = [uint(0x0000000000000000000000000000000000000000000000000000000000000001),
+                                      uint(0x0000000000000000000000000000000000000000000000000000000000000002), 
+                                      uint(0x0b8fe2e8c0659b7501b6315a7ccb684766f67f40221968a92ea8872463d27f2e)];
+        
+        PuzzleLib.Puzzle memory puzzle = puzzleGame.getPuzzle(0);
+        assertEq(puzzle.numSolvers, 0);
+        puzzleGame.submitProof(puzzleId, puzzleProof);
+        
+        puzzle = puzzleGame.getPuzzle(0);
+        assertEq(puzzle.numSolvers, 1);
     }
 
     // test submit proof
@@ -32,16 +92,40 @@ contract PuzzleGameTest is Test {
         puzzleGame.createPuzzle(puzzleType, puzzleData, solutionCommitment, maxSolvers);
 
         uint puzzleId = 0;
-        uint[3] memory puzzleProof = [uint(0x0000000000000000000000000000000000000000000000000000000000000001),
-                                      uint(0x0000000000000000000000000000000000000000000000000000000000000002), 
-                                      uint(0x023d2d04070d863e73cdc6fb1981c26154d77231c55499e0786cdab3bf8f07ad)];
+        uint[3] memory puzzleProof = [uint(0x0f0fc57fa5f2d70244fb9516b9789429b9f39eaafe35baec27b78cef4f76f408),
+                                      uint(0x13996c55f0ac2d73f3654822cb2541cea4863a4d7615e12aa75a30f26340d656), 
+                                      uint(0x15bc5ac0ac210564bce8119fa0ed6324888645e848234d6ff9de3224890f4aee)];
+        
+        PuzzleLib.Puzzle memory puzzle = puzzleGame.getPuzzle(0);
+        assertEq(puzzle.numSolvers, 0);
         puzzleGame.submitProof(puzzleId, puzzleProof);
         
-        // assertEq(puzzleGame.puzzles(0).solvers(0), address(this));
+        puzzle = puzzleGame.getPuzzle(0);
+        assertEq(puzzle.numSolvers, 1);
     }
 
-    // function testSetNumber(uint256 x) public {
-    //     counter.setNumber(x);
-    //     assertEq(counter.number(), x);
-    // }
+       // test submit proof
+    function test_submitProofTwice() public {
+        string memory puzzleType = "crossword_type";
+        string memory puzzleData = "hints";
+        uint[2] memory solutionCommitment = [uint(0x0e074fdf0466838ef0a305e5718b96bd7481e02a5e1106ae7a80236f05db8fb7), 
+                                             uint(0x192f7ca3094b5962f681bb17e18d9bfad6235aa57f3ea2cc4dcdd6c7335f897c)];
+        uint maxSolvers = 1;
+        puzzleGame.createPuzzle(puzzleType, puzzleData, solutionCommitment, maxSolvers);
+
+        uint puzzleId = 0;
+        uint[3] memory puzzleProof = [uint(0x0f0fc57fa5f2d70244fb9516b9789429b9f39eaafe35baec27b78cef4f76f408),
+                                      uint(0x13996c55f0ac2d73f3654822cb2541cea4863a4d7615e12aa75a30f26340d656), 
+                                      uint(0x15bc5ac0ac210564bce8119fa0ed6324888645e848234d6ff9de3224890f4aee)];
+        
+        PuzzleLib.Puzzle memory puzzle = puzzleGame.getPuzzle(0);
+        assertEq(puzzle.numSolvers, 0);
+        puzzleGame.submitProof(puzzleId, puzzleProof);
+        
+        puzzle = puzzleGame.getPuzzle(0);
+        assertEq(puzzle.numSolvers, 1);
+
+        vm.expectRevert("Solver has already solved this puzzle");
+        puzzleGame.submitProof(puzzleId, puzzleProof);
+    }
 }
